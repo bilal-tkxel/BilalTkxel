50947096de7140d6f165a95c08c68bbcadf0e0c9
Bilal Ali Khan <bilal.ali@tkxel.io>
2023-04-11 11:48:49 +0500

    slider fix

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -28,4 +28,20 @@ $('.center').slick({
   arrows: false,
   autoplay: true,
   autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    }
+]
 });
